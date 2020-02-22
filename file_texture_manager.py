8e85b4b97441099d0664ca7be20158f8a02db972
ryanarnanjaya <rodalisme@gmail.com>
2020-02-22 00:30:57 -0800

    is_file_append(), file_exist(), and get_project_path()

--- file_texture_manager.py
+++ file_texture_manager.py
@@ -36,3 +36,37 @@ import maya.cmds as mc
 #     print path
 
 ### print out log of all edits (mike)
+
+# Ryan
+def is_file_append(path, exist_list, lost_list):
+    '''
+    validate if a single path exists and append it to a list
+    '''
+    if os.path.isfile(path):
+        exist_list.append(path)
+        return True
+    else:
+        lost_list.append(path)
+        return False
+
+def file_exist(paths):
+    '''
+    validate input file paths
+    '''
+    paths_exist = list()
+    paths_lost = list()
+    # if multiple paths in a list
+    if isinstance(paths, list):
+        for path in paths:
+            is_file_append(path, paths_exist, paths_lost)
+    # if it is a single path
+    else:
+        is_file_append(path, paths_exist, paths_lost)
+    return (paths_exist, paths_lost)
+
+def get_project_path():
+    '''
+    return current project path
+    '''
+    project_path = mc.workspace(query=True, rootDirectory=True)
+    return project_path
