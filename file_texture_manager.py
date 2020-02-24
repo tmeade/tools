91ded473b0e0a33405110c4a22a5a0e6ec9b3026
tmeade <tmeade@mac.com>
2020-02-23 22:10:03 -0800

    Added logging and __main__ function

--- file_texture_manager.py
+++ file_texture_manager.py
@@ -2,11 +2,11 @@
 A module containing helpful house keeping utilities for managing textures.
 """
 import os.path
-import os.shutil
+import shutil
 import logging
 import maya.cmds as mc
 
-_logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 def get_file_textures():
@@ -15,9 +15,12 @@ def get_file_textures():
         Gets all file textures in a Maya scene
     Parameters: None
     Returns:
-        list: A list of all file testures.
+        list: A list of all file textures.
     '''
-    return mc.ls(type='file')
+    file_texture_nodes = mc.ls(type='file')
+    logging.debug('file_texture_nodes: {}'.format(file_texture_nodes))
+
+    return file_texture_nodes
 
 
 # TODO: Create object to store attributes
@@ -30,6 +33,7 @@ def get_file_texture_attributes(file_node):
     Returns:
         dict: A dictionary of atttribtue name and value pairs ({attr:value})
     '''
+    logging.debug('file_node: {}'.format(file_node))
     # TODO: Check file_node type and assert if not type fileNode
 
     # Create dictionary of attributes from the maya file node
@@ -38,7 +42,8 @@ def get_file_texture_attributes(file_node):
                             'file_texture_name': mc.getAttr('{}.fileTextureName'.format(file_node))
                             })
 
-    # Possibly add other useful data such as connections to other nodes
+    # TODO: Possibly add other useful data such as connections to other nodes
+    logging.debug('file_texture_attribues: {}'.format(file_texture_attribues))
 
     return file_texture_attribues
 
@@ -100,3 +105,14 @@ def get_project_path():
     '''
     project_path = mc.workspace(query=True, rootDirectory=True)
     return project_path
+
+
+# Test code to run when calling module
+# example:
+#   execfile('/Users/tmeade/Documents/python/maya/tools/file_texture_manager.py')
+if __name__ == "__main__":
+    file_texture_nodes = ftm.get_file_textures()
+
+    file_texture_attributes = list()
+    for node in file_texture_nodes:
+        file_texture_attributes.append(ftm.get_file_texture_attributes(node))
